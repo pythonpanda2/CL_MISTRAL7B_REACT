f580a7bd09503d88f98fa0140a37b7a2d29c94ac
Ganesh Sivaraman <46133994+pythonpanda2@users.noreply.github.com>
2024-09-26 22:03:05 -0500

    Add files via upload
    Fine tuning Mistral 7B on Suzuki Coupling dataset

---
+++ CL_LLM_REACT/Mistral_7B_Fine_Tune.py
@@ -0,0 +1,208 @@
+import jax
+import jax.numpy as jnp
+import equinox as eqx
+import json
+#import sentencepiece as spm
+from tokenizer import MistralTokenizer
+from model_customized import Transformer
+from typing import Optional, Tuple, List, NamedTuple
+from jax import random, grad, jit
+from sklearn.model_selection import train_test_split
+from preprocess_Suzuki_Coupling_data import make_reaction 
+from rope import precompute_frequencies
+import os
+import torch  
+from torch.utils.data import Dataset, DataLoader
+import numpy as np 
+import optax
+
+# Mistral-7B Model setting
+class ModelArgs(NamedTuple):
+    dim: int
+    n_layers: int
+    n_heads: int
+    n_kv_heads: int
+    head_dim: int
+    hidden_dim: int
+    vocab_size: int
+    sliding_window: int
+    norm_eps: float
+    max_batch_size: int = 32
+
+
+#Load the pre-trained Mistral model, tokenize inputs
+class Mistral7B:
+    def __init__(self, mistralpath, key, dtype=jnp.bfloat16):
+        self.mistralpath = mistralpath
+        self.mistral_param_path = os.path.join(mistralpath, 'params.json')
+        self.mistral_pretrained_path = os.path.join(mistralpath, 'mistral7B_jax_port_fast.eqx')
+        self.tokenizer_path = os.path.join(mistralpath, 'tokenizer.model')
+        self.sp = MistralTokenizer(self.tokenizer_path)
+        with open(self.mistral_param_path, "r") as f:
+            self.args = ModelArgs(**json.loads(f.read()))
+        self.mistral_model = Transformer(self.args, key, dtype)
+
+    def get_mistral_pretrained(self):
+        return eqx.tree_deserialise_leaves(self.mistral_pretrained_path , self.mistral_model)
+
+    def tokenize_smiles(self, smiles):
+        return self.sp.encode(smiles)
+
+    def _precompute(self,max_len):
+        cache_k = jnp.zeros((self.args.max_batch_size, self.args.n_layers, self.args.sliding_window, self.args.n_kv_heads, self.args.head_dim), dtype=jnp.bfloat16)
+        cache_v = jnp.zeros((self.args.max_batch_size, self.args.n_layers, self.args.sliding_window, self.args.n_kv_heads, self.args.head_dim), dtype=jnp.bfloat16)
+        cos_freq, sin_freq = precompute_frequencies(self.args.head_dim, max_len)
+        positions = jnp.arange(0, max_len)
+        positions_padded = jnp.pad( positions, (0, self.args.sliding_window - len(positions)), constant_values=-1)
+        return cache_k, cache_v, cos_freq, sin_freq, positions_padded
+
+
+# Define the regression block with a Multi-head attention layer
+class MultiHeadAttentionRegression(eqx.Module):
+    mha: eqx.nn.MultiheadAttention
+    ffn: eqx.nn.Sequential
+    rmsnorm1: eqx.nn.RMSNorm
+    rmsnorm2: eqx.nn.RMSNorm
+    linear1: eqx.nn.Linear
+    linear2: eqx.nn.Linear
+    output_layer: eqx.nn.Linear
+
+    def __init__(self, num_heads, embed_dim, key):
+
+        assert embed_dim > 0, "embed_dim must be a positive integer"
+        
+        # Split key for reproducibility
+        subkey_mha, subkey_ffn, subkey_linear1, subkey_linear2, subkey_output = jax.random.split(key, 5)
+
+        self.mha = eqx.nn.MultiheadAttention(num_heads=num_heads, query_size=embed_dim, dtype=jnp.float32,  key=subkey_mha)
+        self.ffn = eqx.nn.MLP(
+            embed_dim,
+            out_size=embed_dim,
+            width_size= embed_dim,
+            depth=2,
+            activation = jax.nn.silu,
+            key=subkey_ffn,
+        )
+        self.rmsnorm1 = eqx.nn.RMSNorm(shape=embed_dim,  dtype=jnp.float32)
+        self.rmsnorm2 = eqx.nn.RMSNorm(shape=embed_dim,  dtype=jnp.float32)
+        self.linear1 = eqx.nn.Linear(embed_dim, 256, key=subkey_linear1, dtype=jnp.float32)
+        self.linear2 = eqx.nn.Linear(256, 128, key=subkey_linear2, dtype=jnp.float32)
+        self.output_layer = eqx.nn.Linear(128, 1, key=subkey_output,  dtype=jnp.float32)
+
+    def __call__(self, x):
+        #x = mistral(x)
+        x =  x + self.mha(x, x, x)  # Residual connection
+        x = jax.vmap(self.rmsnorm1)(x)
+        ff = jax.vmap(self.ffn)(x)
+        x = x + ff
+        x = jax.vmap(self.rmsnorm2)(x)
+        x = jax.vmap(self.linear1)(x)
+        x = jax.nn.silu(x)
+        x = jax.vmap(self.linear2)(x)
+        x = jax.nn.silu(x)
+        return jax.vmap(self.output_layer)(x)  # Output a scalar value
+
+class ReactionDataset(Dataset):
+    def __init__(self, rxn, yields):
+        self.rxn = rxn
+        self.yields = yields
+
+    def __len__(self):
+        return len(self.rxn)
+
+    def __getitem__(self, idx):
+        return self.rxn[idx], self.yields[idx]
+
+#Test these classes below!
+key=jax.random.PRNGKey(0)
+path="/lcrc/project/FOUND4CHEM/MISTRAL_CL_CONDA/mistral_jax/model_files"
+
+Mistral = Mistral7B(path,key) #<---- Class
+mistral_model = Mistral.get_mistral_pretrained() #<---- Actual model weights!
+
+# Data Loading and Preprocessing
+data_file  = "/lcrc/project/FOUND4CHEM/projects/Mistral_7B/data/Suzuki-Miyaura/aap9112_Data_File_S1.xlsx"
+df = make_reaction(data_file) #df[['rxn', 'y']]
+#tokenized_smiles, yields, max_len = preprocess_data(df)
+
+tokenized_rxn = [Mistral.tokenize_smiles(smiles) for smiles in df['rxn']]
+max_len =     max(len(sublist) for sublist in tokenized_rxn) #len(tokenized_rxn[0])
+padded_a = [sublist + [0] * (max_len - len(sublist)) for sublist in tokenized_rxn]
+inp_array = np.stack([np.array(aa) for aa in padded_a]) # jnp.array(padded_a)
+yields = np.array(df['y'], dtype=jnp.float32)
+
+cache_k, cache_v, cos_freq, sin_freq, positions_padded = Mistral._precompute(max_len)
+
+# 80/20 split
+train_rxn, val_rxn, train_yields, val_yields = train_test_split(
+    inp_array, yields, test_size=0.2, random_state=42
+)
+
+print(train_rxn.shape, type(train_rxn),  val_rxn.shape)
+# Convert JAX arrays to PyTorch tensors
+train_rxns_torch = torch.tensor(train_rxn)
+train_yields_torch = torch.tensor(train_yields, dtype=torch.float32)
+val_rxns_torch = torch.tensor(val_rxn)
+val_yields_torch = torch.tensor(val_yields, dtype=torch.float32)
+
+# Create dataset objects for training and validation
+train_dataset = ReactionDataset(train_rxns_torch, train_yields_torch)
+val_dataset = ReactionDataset(val_rxns_torch, val_yields_torch)
+
+print("\n Batch size:",Mistral.args.max_batch_size)
+train_loader = DataLoader(train_dataset, batch_size=Mistral.args.max_batch_size, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=Mistral.args.max_batch_size, shuffle=False)
+
+# Initialize the regression block
+embed_dim = 4096  # Assuming embedding dimension of Mistral
+num_heads = 1  # Single attention head for regression task
+predictor = MultiHeadAttentionRegression(num_heads, embed_dim,  key)
+
+
+# Step 1: Fine-tune the model
+@eqx.filter_value_and_grad
+def loss_fn(predictor, model,  batch_rxns, batch_yields, cache_k, cache_v, cos_freq, sin_freq, positions_padded ):
+
+    batch_rxns = jnp.array(batch_rxns.numpy())
+    batch_yields = jnp.array(batch_yields.numpy())
+   
+    model = jax.vmap(model, in_axes= (0, None, None, None, None, 0, 0))
+    embeddings, cache_k, cache_v = model(batch_rxns, cos_freq, sin_freq, positions_padded, None, cache_k, cache_v)
+    print("\nEmbedding shape: ",embeddings.shape)
+    predictions = jax.vmap(predictor)(embeddings)
+
+    loss = jnp.mean((predictions - batch_yields) ** 2)
+    return loss
+
+#Step 2 : Training step 
+@eqx.filter_jit
+def train_step(predictor, model, batch_rxns, batch_yields, cache_k, cache_v, cos_freq, sin_freq, positions_padded, opt_state):
+    # Get loss and gradients
+    loss, grads = loss_fn(predictor, model, batch_rxns, batch_yields, cache_k, cache_v, cos_freq, sin_freq, positions_padded)
+    # Update the parameters using the optimizer
+    updates, opt_state = optimizer.update(grads, opt_state, predictor)
+    predictor = eqx.apply_updates(predictor, updates)
+    
+    return loss, predictor, opt_state
+
+
+# Step 3: Initialize optimizer
+optimizer = optax.adamw(learning_rate=1e-4)
+opt_state = optimizer.init(eqx.filter(predictor, eqx.is_array))  # optimizer.init(predictor)
+
+num_epochs = 2
+step = 1  
+# Training Loop with DataLoader
+for epoch in range(num_epochs):
+    running_loss = 0.0
+    for batch_rxn, batch_yields in train_loader:
+        # Perform a training step
+        loss, predictor, opt_state  = train_step( predictor, mistral_model,  batch_rxn, batch_yields, cache_k, cache_v, cos_freq, sin_freq, positions_padded, opt_state)
+        
+        loss = loss.item()
+        print(f"step={epoch}, loss={loss}")
+        # Accumulate loss for monitoring
+        running_loss += loss
+       
+    # Print epoch loss
+    print(f"Epoch {epoch}, Loss: {running_loss / len(train_loader)}")
---
+++ CL_LLM_REACT/model_customized.py
@@ -0,0 +1,287 @@
+import jax
+import jax.numpy as jnp
+
+import equinox as eqx
+from equinox._misc import default_floating_dtype
+from jaxtyping import Array, Float
+from typing import Tuple
+
+from rope import calculate_rope
+
+
+class RMSNorm(eqx.Module):
+    eps: float
+    weight: Float[Array, "*shape"]  # noqa: F821
+
+    def __init__(self, dim, eps, dtype=jnp.bfloat16):
+        dtype = default_floating_dtype() if dtype is None else dtype
+        self.eps = eps
+        self.weight = jnp.ones(shape=dim, dtype=dtype)
+
+    def _norm(self, x):
+        return x * jax.lax.rsqrt(jnp.mean(x**2, keepdims=True) + self.eps)
+
+    def __call__(self, x):
+        output = self._norm(x.astype(jnp.float32)).astype(x.dtype)
+        return output * self.weight
+
+
+class FeedForward(eqx.Module):
+    w1: eqx.nn.Linear
+    w2: eqx.nn.Linear
+    w3: eqx.nn.Linear
+
+    def __init__(self, args, key, dtype=jnp.bfloat16):
+        dtype = default_floating_dtype() if dtype is None else dtype
+        key1, key2, key3 = jax.random.split(key, 3)
+
+        self.w1 = eqx.nn.Linear(
+            args.dim, args.hidden_dim, use_bias=False, key=key1, dtype=dtype
+        )
+        self.w2 = eqx.nn.Linear(
+            args.hidden_dim, args.dim, use_bias=False, key=key2, dtype=dtype
+        )
+        self.w3 = eqx.nn.Linear(
+            args.dim, args.hidden_dim, use_bias=False, key=key3, dtype=dtype
+        )
+
+    def __call__(self, x):
+        h = jax.nn.silu(self.w1(x).astype(jnp.float32)).astype(x.dtype)
+        return self.w2(h * self.w3(x))
+
+
+class Attention(eqx.Module):
+    dim: int
+    n_heads: int
+    head_dim: int
+    n_kv_heads: int
+    kv_repeats: int
+    sliding_window: int
+    scale: float
+    split_sizes: Tuple
+    wqkv: eqx.nn.Linear
+    wo: eqx.nn.Linear
+
+    def __init__(self, args, key, dtype=jnp.bfloat16):
+        dtype = default_floating_dtype() if dtype is None else dtype
+        key1, key2 = jax.random.split(key, 2)
+
+        self.n_heads = args.n_heads
+        self.head_dim = args.head_dim
+        self.n_kv_heads = args.n_kv_heads
+        self.dim = args.dim
+        self.kv_repeats = self.n_heads // self.n_kv_heads
+        self.sliding_window = args.sliding_window
+        self.scale = args.head_dim**-0.5
+        total_head_dim = (args.n_heads + 2 * args.n_kv_heads) * args.head_dim
+        self.split_sizes = (
+            args.n_heads * args.head_dim,
+            (args.n_heads * args.head_dim) + (args.n_kv_heads * args.head_dim),
+        )
+
+        self.wqkv = eqx.nn.Linear(
+            args.dim, total_head_dim, use_bias=False, key=key1, dtype=dtype
+        )
+        self.wo = eqx.nn.Linear(
+            args.n_heads * args.head_dim,
+            args.dim,
+            use_bias=False,
+            key=key2,
+            dtype=dtype,
+        )
+
+    def compute_scores_and_output(self, xq, key, value, mask, seqlen, pos_mask):
+        query = jnp.transpose(xq, (1, 0, 2))
+        key = jnp.transpose(key, (1, 0, 2))
+        value = jnp.transpose(value, (1, 0, 2))
+
+        # # # scores : [n_heads, seqlen | 1, seqlen]
+        scores = jnp.matmul(query, jnp.transpose(key, (0, 2, 1))) * self.scale
+        if pos_mask is not None:
+            scores = jnp.where(pos_mask, -jnp.inf, scores)
+
+        if mask is not None:
+            # Mask will of shape [seqlen, seqlen] but our scores
+            # have shape [num_heads, seqlen, seqlen], hence we need
+            # to introduce another dimension in the mask
+            mask = mask[jnp.newaxis, ...]
+            scores = scores + mask
+
+        scores = jax.nn.softmax(scores.astype(jnp.float32), axis=-1).astype(query.dtype)
+        output = jnp.matmul(scores, value)
+        output = jnp.reshape(jnp.transpose(output, (1, 0, 2)), (seqlen, -1))
+        output = jax.vmap(self.wo)(output)
+        return output
+
+    def __call__(
+        self, x, cos_freq, sin_freq, positions, mask=None, cache_k=None, cache_v=None
+    ):
+        # x shape: [seqlen, embed_dim]
+        seqlen = x.shape[0]
+        print(x.shape)
+        xqkv = jax.vmap(self.wqkv)(x)
+        xq, xk, xv = jnp.split(xqkv, self.split_sizes, axis=-1)
+
+        xq = jnp.reshape(xq, (seqlen, self.n_heads, self.head_dim))
+        xk = jnp.reshape(xk, (seqlen, self.n_kv_heads, self.head_dim))
+        xv = jnp.reshape(xv, (seqlen, self.n_kv_heads, self.head_dim))
+
+        xq = calculate_rope(xq, cos_freq, sin_freq)
+        xk = calculate_rope(xk, cos_freq, sin_freq)
+
+        if positions.shape[0] > 1:
+            # prefill
+            cache_k = cache_k.at[positions, :, :].set(xk[positions, :, :], mode="drop")
+            cache_v = cache_v.at[positions, :, :].set(xv[positions, :, :], mode="drop")
+
+            key = jnp.repeat(xk, self.kv_repeats, axis=1)
+            value = jnp.repeat(xv, self.kv_repeats, axis=1)
+            output = self.compute_scores_and_output(xq, key, value, mask, seqlen, None)
+        else:
+            # single-token generation
+            one_hot_indices = jax.nn.one_hot(
+                positions, self.sliding_window, dtype=cache_k.dtype
+            ).reshape(self.sliding_window, 1, 1)
+            # Ideally, we expect that you flush out the cache with the new prompt,
+            # and start over. Why flushing out the cache values are necessary?
+            # It ensures that we are not adding any values updated earlier with the
+            # new updates, meaning we are always replacing the value not updating it.
+            # For example, if prompt had a length of 6, and you want to generate 7th
+            # token, this ensures that we are not adding the old value of 7th token
+            # to the updated value as it would lead to wrong results.
+            # In case, you are not flushing the cache after every prompt, add the
+            # `jnp.where()` condition as shown below:
+            # k_updates = cache_k + xk * one_hot_indices
+            # v_updates = cache_v + xv * one_hot_indices
+            # cache_k = jnp.where(cache_k, cache_k, k_updates)
+            # cache_v = jnp.where(cache_v, cache_v, v_updates)
+            cache_k = cache_k + xk * one_hot_indices
+            cache_k = cache_v + xv * one_hot_indices
+
+            cur_pos = positions[-1] + 1
+            causal_mask = jnp.broadcast_to(
+                jnp.arange(self.sliding_window) >= cur_pos, (1, 1, self.sliding_window)
+            ).reshape(self.sliding_window, 1, 1)
+
+            key = jnp.repeat(
+                jnp.where(causal_mask, 0, cache_k), axis=1, repeats=self.kv_repeats
+            )
+            value = jnp.repeat(
+                jnp.where(causal_mask, 0, cache_v), axis=1, repeats=self.kv_repeats
+            )
+
+            output = self.compute_scores_and_output(
+                xq,
+                key,
+                value,
+                mask,
+                seqlen,
+                causal_mask.reshape((1, 1, self.sliding_window)),
+            )
+
+        return output, cache_k, cache_v
+
+
+class TransformerBlock(eqx.Module):
+    dim: int
+    n_heads: int
+    attention: Attention
+    attention_norm: RMSNorm
+    feed_forward: FeedForward
+    ffn_norm: RMSNorm
+
+    def __init__(self, args, key, dtype=jnp.bfloat16):
+        key1, key2 = jax.random.split(key, 2)
+        self.n_heads = args.n_heads
+        self.dim = args.dim
+
+        self.attention = Attention(args, key=key1, dtype=dtype)
+        self.attention_norm = RMSNorm(args.dim, eps=args.norm_eps, dtype=dtype)
+
+        self.feed_forward = FeedForward(args, key=key2, dtype=dtype)
+        self.ffn_norm = RMSNorm(args.dim, eps=args.norm_eps, dtype=dtype)
+
+    def __call__(self, x, cos_freq, sin_freq, positions, mask, cache_k, cache_v):
+        normed_x = jax.vmap(self.attention_norm)(x)
+        r, cache_k, cache_v = self.attention(
+            normed_x, cos_freq, sin_freq, positions, mask, cache_k, cache_v
+        )
+        h = x + r
+        r = jax.vmap(self.feed_forward)(jax.vmap(self.ffn_norm)(h))
+        out = h + r
+        return out, cache_k, cache_v
+
+
+class Transformer(eqx.Module):
+    tok_embeddings: eqx.nn.Embedding
+    layers: TransformerBlock
+    norm: RMSNorm
+    output: eqx.nn.Linear
+    vocab_size: int
+    n_layers: int
+    sliding_window: int
+
+    def __init__(self, args, key, dtype=jnp.bfloat16):
+        self.vocab_size = args.vocab_size
+        self.n_layers = args.n_layers
+        self.sliding_window = args.sliding_window
+        keys = jax.random.split(key, args.n_layers + 2)
+        embed_key, linear_key, tf_layers_keys = keys[0], keys[1], keys[2:]
+
+        self.tok_embeddings = eqx.nn.Embedding(
+            args.vocab_size, args.dim, key=embed_key, dtype=dtype
+        )
+        self.norm = RMSNorm(dim=args.dim, eps=args.norm_eps, dtype=dtype)
+        self.output = eqx.nn.Linear(
+            args.dim, args.vocab_size, use_bias=False, key=linear_key, dtype=dtype
+        )
+        make_layers = lambda k: TransformerBlock(args, key=k, dtype=dtype)
+        self.layers = eqx.filter_vmap(make_layers)(tf_layers_keys)
+        del make_layers
+
+    def compute_mask(self, seqlen):
+        t = jnp.full((seqlen, seqlen), dtype=jnp.bfloat16, fill_value=1)
+        mask = jnp.tril(t, k=0)
+        # make the mask banded to account for sliding window
+        mask = jnp.log(jnp.triu(mask, k=-self.sliding_window))
+        return mask
+
+    def __call__(self, x, cos_freq, sin_freq, positions, mask, cache_k, cache_v):
+        # x is of shape (seqlen, )
+        h = jax.vmap(self.tok_embeddings)(x)
+        #h = jax.vmap(jax.vmap(self.tok_embeddings))(x)
+
+        if x.shape[-1] > 1:
+            seqlen = x.shape[-1]
+            mask = self.compute_mask(seqlen)
+        else:
+            mask = None
+
+        # Partition the TransformerLayers into static and dynamic parts
+        dynamic_layers, static_layers = eqx.partition(self.layers, eqx.is_array)
+
+        def f(_x, _dynamic_l):
+            layer = eqx.combine(_dynamic_l, static_layers)
+            h, cache_k, cache_v, layer_idx = _x
+            h, cache_ki, cache_vi = layer(
+                h,
+                cos_freq,
+                sin_freq,
+                positions,
+                mask,
+                cache_k[layer_idx, ...],
+                cache_v[layer_idx, ...],
+            )
+            cache_k = cache_k.at[layer_idx, :, :, :].set(cache_ki)
+            cache_v = cache_v.at[layer_idx, :, :, :].set(cache_vi)
+            return (h, cache_k, cache_v, layer_idx + 1), None
+
+        layer_idx = 0
+        print(h.shape, cache_k.shape, cache_v.shape, layer_idx)
+        (h, cache_k, cache_v, layer_idx), _ = jax.lax.scan(
+            f, (h, cache_k, cache_v, layer_idx), dynamic_layers
+        )
+
+        h = jax.vmap(self.norm)(h).astype(jnp.float32)
+        #h = jax.vmap(self.output)(h).astype(jnp.float32)
+        return h, cache_k, cache_v
